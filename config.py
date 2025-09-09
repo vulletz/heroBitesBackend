28bdc52acd7fc2071092a193689c9232df454003
vulletz <122696042+vulletz@users.noreply.github.com>
2025-09-08 19:23:26 -0600

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+# backend/config.py
+
+MARVEL_PUBLIC_KEY = "e3aa0ae65d490e403a46e40b6af223bb"
+MARVEL_PRIVATE_KEY = "10fe4943a82545b65026c3e44f33c768cc54c1f3"
+SPOONACULAR_API_KEY = "ec4593e1dc234702904a7dfff022245f"
+BASE_URL_MARVEL = "https://gateway.marvel.com/v1/public"
+BASE_URL_SPOONACULAR = "https://api.spoonacular.com"
